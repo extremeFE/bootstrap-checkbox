292091f00b0f57700adb50268d834ac327625f06
jiung kang <trkang79@gmail.com>
2013-06-28 15:43:19 +0900

    add checkboxVal, checked function

--- js/bootstrap-checkbox.js
+++ js/bootstrap-checkbox.js
@@ -7,30 +7,31 @@
 (function($) {
   "use strict";
 
-  var replaceCheckboxElement = function(element) {
+  var replaceCheckboxElement = function(element, checkbox) {
     var value = element.val(),
+        className = element.attr('class'),
+        style = element.attr('style'),
         checked = !!element[0].checked,
-        welNew = $('<div class="bootstrap-checkbox"></div>');
+        welNew = $('<div></div>');
 
     element.replaceWith(welNew);
 
-    if (checked) {
-      welNew.addClass('checked');
-    }
+    if (className) { welNew.attr('class', className) }
+    welNew.addClass('bootstrap-checkbox');
+    if (style) { welNew.attr('style', style); }
+    if (checked) { welNew.addClass('checked'); }
 
-    welNew.attr('data-value', value);
-    welNew.attr('data-checked', checked);
-
-    return welNew;
+    checkbox.value = value;
+    checkbox.checked = checked;
+    checkbox.element = welNew;
   };
 
   var attachEvent = function(checkbox) {
     var element = checkbox.element;
     element.on('click', function(e) {
-      var value = element.attr('data-value');
-      var checked = !(element.attr('data-checked') == 'true');
+      var checked = !checkbox.checked;
+      checkbox.checked = checked;
 
-      element.attr('data-checked', checked);
       if (checked) {
         element.addClass('checked');
       } else {
@@ -39,31 +40,69 @@
 
       element.trigger({
         type: 'check',
-        value: value,
+        value: checkbox.value,
         checked: checked,
-        element: checkbox.element
+        element: element
       });
     });
   };
 
   var Checkbox = function(element) {
-    this.element = replaceCheckboxElement(element);
+    replaceCheckboxElement(element, this);
+
     attachEvent(this);
   };
 
   $.fn.extend({
-    checkbox : function(options) {
-      return $(this.map(function () {
+    checkbox : function(option) {
+      var result = $(this.map(function () {
         var $this = $(this),
             data = $this.data('checkbox');
 
         if (!data) {
-          data = new Checkbox($this, options)
+          data = new Checkbox($this);
           data.element.data('checkbox', data);
         }
+        if (typeof option == 'string') data[option]()
 //        data.ambiguous = !!(options && options.ambiguous);
-        return data.element;
+        return data.element[0];
       }));
+      result.selector = this.selector;
+      return result;
+    },
+
+    checkboxVal : function(value) {
+      var $this = $(this[0]);
+      var data = $this.data('checkbox');
+
+      if (!data) {
+        return;
+      }
+      if ($.type(value) === "undefined") {
+        return data.value;
+      } else {
+        data.value = value;
+        $this.data('checkbox', data);
+      }
+    },
+
+    checked : function(checked) {
+      var $this = $(this[0]);
+      var data = $this.data('checkbox');
+      if (!data) {
+        return;
+      }
+      if ($.type(checked) === "undefined") {
+        return data.checked;
+      } else {
+        if (checked ) {
+          $this.addClass('checked');
+        } else {
+          $this.removeClass('checked');
+        }
+        data.checked = checked;
+        $this.data('checkbox', data);
+      }
     }
   });
 })(jQuery);
